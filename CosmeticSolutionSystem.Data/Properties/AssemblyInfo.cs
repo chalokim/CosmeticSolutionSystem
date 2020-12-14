e7061def6599ed632961fdfa5a760e298e68b312
SieonLee <74527137+SieonLee@users.noreply.github.com>
2020-12-14 18:09:09 +0900

    Data

--- CosmeticSolutionSystem.Data/Properties/AssemblyInfo.cs
+++ CosmeticSolutionSystem.Data/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("043a8451-d7a5-43a2-b74e-50bd9e45cd3f")]
+[assembly: Guid("3180ca16-172e-4e26-8b18-a98967315b93")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
