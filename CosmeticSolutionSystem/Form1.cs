78e279a24d8177a7828f0ffcba5cd98cd98d29b5
SieonLee <74527137+SieonLee@users.noreply.github.com>
2020-12-15 09:41:20 +0900

    Revert "리본 메뉴, 탭 MDI, 차트 화면 추가함"
    This reverts commit c83e25c74969cd8c937486a17c9566da821e7fca.

--- CosmeticSolutionSystem/Form1.cs
+++ CosmeticSolutionSystem/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CosmeticSolutionSystem
 {
-    public partial class MonthlyDailySalesVolume : Form
+    public partial class Form1 : Form
     {
-        public MonthlyDailySalesVolume()
+        public Form1()
         {
             InitializeComponent();
         }
